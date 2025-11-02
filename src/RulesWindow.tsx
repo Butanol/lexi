a218cc7dc40d0dfa0cbd1292dcf9f874316189e1
Butanol <51089545+Butanol@users.noreply.github.com>
2025-11-02 08:44:14 +0800

    Set up Frontend

---
+++ src/RulesWindow.tsx
@@ -0,0 +1,15 @@
+import { Modal } from "@chakra-ui/modal";
+import {Box} from "@chakra-ui/react";
+
+function RulesWindow(isOpen:boolean){
+
+    return (
+        <>
+            <Modal isOpen={isOpen}>
+                <Box></Box>
+            </Modal>
+        </>
+    )
+}
+
+export default RulesWindow;
